563a19bdcad84c4acf9532b6979143a86f5e54fd
Sondre Aasemoen <sondre@eons.io>
2026-07-17 21:49:52 +0200

    Scaffold out a bit more Astro stuff

--- astro.config.ts
+++ astro.config.ts
@@ -3,16 +3,24 @@ import compressor from "astro-compressor";
 
 import sitemap from "@astrojs/sitemap";
 
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   srcDir: "./src",
   base: "/",
   site: "https://cv.eons.io",
   trailingSlash: "always",
   output: "static",
+
   build: {
     format: "directory",
     inlineStylesheets: "never",
   },
+
   compressHTML: true,
   integrations: [sitemap(), compressor()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
